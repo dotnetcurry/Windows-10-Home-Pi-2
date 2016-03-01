51dd2337289735727a9831a439ae9904d9d2c5d8
DotNetCurry <dotnetcurry@a2zknowledgevisuals.com>
2016-03-01 14:55:35 +0530

    homepi code

---
+++ HomePi/Class/Utilities.cs
@@ -0,0 +1,298 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomePi.Class
+{
+    class Utilities
+    {
+        public static async Task<int> GetNextBus()
+        {
+
+            int nextbusIn = 0;
+            try
+            {
+                var response = await GetjsonStream("http://www.wienerlinien.at/ogd_routing/XML_TRIP_REQUEST2?locationServerActive=1&outputFormat=JSON&type_origin=stopid&name_origin=60200884&type_destination=stopid&name_destination=60200641");
+                RootObject obj = JsonConvert.DeserializeObject<RootObject>(response);
+
+                foreach (Trip trip in obj.trips)
+                {
+                    BUSDateTime nxt = trip.trip.legs[0].points[0].dateTime;
+                    DateTime nextBus = DateTime.ParseExact(nxt.time, "HH:mm", System.Globalization.CultureInfo.InvariantCulture);
+
+                    if (DateTime.Compare(DateTime.Now, nextBus) == -1) //Return the first next bus
+                    {
+                        TimeSpan diff = nextBus - DateTime.Now;
+                        int minutes = Convert.ToInt16(Math.Round(diff.TotalMinutes, 0));
+                        if (minutes > 2)
+                        {
+                            nextbusIn = minutes;
+                            return nextbusIn;
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                //Handle error
+            }
+            return nextbusIn;
+        }
+
+        public async static Task<List<SoundCloudTrack>> GetLikes()
+        {
+            List<SoundCloudTrack> likes = new List<SoundCloudTrack>();
+            try
+            {
+                string responseText = await GetjsonStream("http://api.soundcloud.com/" + "users/" + "shoban-kumar" + ".json?client_id=YOUR_CLIENT_ID_HERE");
+                SoundCloudUser user = JsonConvert.DeserializeObject<SoundCloudUser>(responseText);
+                int userId = user.id;
+                responseText = await GetjsonStream("http://api.soundcloud.com/" + "users/" + userId + "/favorites.json?client_id=YOUR_CLIENT_ID_HERE");
+                likes = JsonConvert.DeserializeObject<List<SoundCloudTrack>>(responseText);
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return likes;
+        }
+
+        public static async Task<string> GetjsonStream(string url) //Function to read from given url
+        {
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync(url);
+            HttpResponseMessage v = new HttpResponseMessage();
+            return await response.Content.ReadAsStringAsync();
+        }
+
+    }
+
+    #region Classes
+
+    //For Sound cloud
+    public class SoundCloudUser
+    {
+        public int id { get; set; }
+        public string permalink { get; set; }
+        public string username { get; set; }
+        public string uri { get; set; }
+        public string permalink_url { get; set; }
+        public string avatar_url { get; set; }
+    }
+    public class SoundCloudTrack
+    {
+        public int id { get; set; }
+        public string created_at { get; set; }
+        public int user_id { get; set; }
+        public int duration { get; set; }
+        public bool commentable { get; set; }
+        public string state { get; set; }
+        public string sharing { get; set; }
+        public string tag_list { get; set; }
+        public string permalink { get; set; }
+        public object description { get; set; }
+        public bool streamable { get; set; }
+        public bool downloadable { get; set; }
+        public object genre { get; set; }
+        public object release { get; set; }
+        public object purchase_url { get; set; }
+        public object label_id { get; set; }
+        public object label_name { get; set; }
+        public object isrc { get; set; }
+        public object video_url { get; set; }
+        public string track_type { get; set; }
+        public object key_signature { get; set; }
+        public object bpm { get; set; }
+        public string title { get; set; }
+        public object release_year { get; set; }
+        public object release_month { get; set; }
+        public object release_day { get; set; }
+        public string original_format { get; set; }
+        public int original_content_size { get; set; }
+        public string license { get; set; }
+        public string uri { get; set; }
+        public string permalink_url { get; set; }
+        public object artwork_url { get; set; }
+        public string waveform_url { get; set; }
+        public SoundCloudUser user { get; set; }
+        public string stream_url { get; set; }
+        public string download_url { get; set; }
+        public int playback_count { get; set; }
+        public int download_count { get; set; }
+        public int favoritings_count { get; set; }
+        public int comment_count { get; set; }
+        public SoundCloudCreatedWith created_with { get; set; }
+        public string attachments_uri { get; set; }
+    }
+    public class SoundCloudCreatedWith
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string uri { get; set; }
+        public string permalink_url { get; set; }
+    }
+
+    //For bus
+    public class Parameter
+    {
+        public string name { get; set; }
+        public string value { get; set; }
+    }
+
+    public class BUSDateTime
+    {
+        public string date { get; set; }
+        public string time { get; set; }
+    }
+
+    public class Stamp
+    {
+        public string date { get; set; }
+        public string time { get; set; }
+    }
+
+    public class Link
+    {
+        public string name { get; set; }
+        public string type { get; set; }
+        public string href { get; set; }
+    }
+
+    public class Ref
+    {
+        public string id { get; set; }
+        public string area { get; set; }
+        public string platform { get; set; }
+        public string NaPTANID { get; set; }
+        public List<object> attrs { get; set; }
+        public string coords { get; set; }
+    }
+
+    public class Point
+    {
+        public string name { get; set; }
+        public string place { get; set; }
+        public string nameWithPlace { get; set; }
+        public string usage { get; set; }
+        public string omc { get; set; }
+        public string placeID { get; set; }
+        public string desc { get; set; }
+        public BUSDateTime dateTime { get; set; }
+        public Stamp stamp { get; set; }
+        public List<Link> links { get; set; }
+        public Ref @ref { get; set; }
+    }
+
+    public class Diva
+    {
+        public string branch { get; set; }
+        public string line { get; set; }
+        public string supplement { get; set; }
+        public string dir { get; set; }
+        public string project { get; set; }
+        public string network { get; set; }
+        public string stateless { get; set; }
+        public string @operator { get; set; }
+        public string opCode { get; set; }
+    }
+
+    public class Mode
+    {
+        public string name { get; set; }
+        public string number { get; set; }
+        public string type { get; set; }
+        public string code { get; set; }
+        public string destination { get; set; }
+        public string destID { get; set; }
+        public string desc { get; set; }
+        public Diva diva { get; set; }
+    }
+
+    public class Ref2
+    {
+        public string id { get; set; }
+        public string area { get; set; }
+        public string platform { get; set; }
+        public string NaPTANID { get; set; }
+        public List<object> attrs { get; set; }
+        public string coords { get; set; }
+        public string depDateTime { get; set; }
+        public string arrDateTime { get; set; }
+    }
+
+    public class StopSeq
+    {
+        public string name { get; set; }
+        public string nameWO { get; set; }
+        public string place { get; set; }
+        public string nameWithPlace { get; set; }
+        public string omc { get; set; }
+        public string placeID { get; set; }
+        public string platformName { get; set; }
+        public Ref2 @ref { get; set; }
+    }
+
+    public class Frequency
+    {
+        public string hasFrequency { get; set; }
+        public string tripIndex { get; set; }
+        public string minTimeGap { get; set; }
+        public string maxTimeGap { get; set; }
+        public string avTimeGap { get; set; }
+        public string minDuration { get; set; }
+        public string maxDuration { get; set; }
+        public string avDuration { get; set; }
+        public List<object> modes { get; set; }
+    }
+
+    public class TurnInst
+    {
+        public string dir { get; set; }
+        public string manoeuvre { get; set; }
+        public string name { get; set; }
+        public string dirHint { get; set; }
+        public string place { get; set; }
+        public string tTime { get; set; }
+        public string ctTime { get; set; }
+        public string dis { get; set; }
+        public string cDis { get; set; }
+        public string coords { get; set; }
+    }
+
+    public class Leg
+    {
+        public List<Point> points { get; set; }
+        public Mode mode { get; set; }
+        public List<StopSeq> stopSeq { get; set; }
+        public Frequency frequency { get; set; }
+        public string path { get; set; }
+        public List<TurnInst> turnInst { get; set; }
+    }
+
+    public class Trip2
+    {
+        public string duration { get; set; }
+        public string interchange { get; set; }
+        public string desc { get; set; }
+        public List<Leg> legs { get; set; }
+        public List<object> attrs { get; set; }
+    }
+
+    public class Trip
+    {
+        public Trip2 trip { get; set; }
+    }
+
+    public class RootObject
+    {
+        public List<Parameter> parameters { get; set; }
+        public List<Trip> trips { get; set; }
+    }
+    #endregion
+
+}
---
+++ HomePi/MainPage.xaml.cs
@@ -0,0 +1,389 @@
+﻿using HomePi.Class;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Windows.Devices.Spi;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Automation.Provider;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace HomePi
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        enum CurrentPage
+        {
+            Weather,
+            NextBus,
+            MusicPlayer,
+            Power
+        };
+
+        string tempC = string.Empty;
+        DispatcherTimer appTimer, touchTimer;
+        const string CalibrationFilename = "TSC2046";
+        Windows.Foundation.Point lastPosition = new Windows.Foundation.Point(double.NaN, double.NaN);
+        int nextBus = 0, nowPlaying = 0;
+        CurrentPage currenPage;
+        List<SoundCloudTrack> likes = new List<SoundCloudTrack>();
+
+        const Int32 DCPIN = 22;
+        const Int32 RESETPIN = 27;
+
+        ILI9341Display display1 = new ILI9341Display(ILI9341Display.BLACK, DCPIN, RESETPIN);
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            Init();
+            base.OnNavigatedTo(e);
+        }
+
+        private void initTimers()
+        {
+            appTimer = new DispatcherTimer();
+            appTimer.Tick += AppTimer_Tick;
+            appTimer.Interval = TimeSpan.FromMinutes(1);
+            appTimer.Start();
+
+            touchTimer = new DispatcherTimer();
+            touchTimer.Tick += TouchTimer_Tick;
+            touchTimer.Interval = TimeSpan.FromMilliseconds(50);
+            touchTimer.Start();
+
+        }
+
+        private void TouchTimer_Tick(object sender, object e)
+        {
+            TSC2046.CheckTouch();
+
+            int x = TSC2046.getTouchX();
+            int y = TSC2046.getTouchY();
+            int p = TSC2046.getPressure();
+
+            if (p > 5)
+            {
+                CheckAction(TSC2046.getDispX(), TSC2046.getDispY());
+            }
+        }
+
+        private async void AppTimer_Tick(object sender, object e)
+        {
+            //Run only if the current page is Next Bus
+            switch (currenPage)
+            {
+                case CurrentPage.NextBus:
+                    //Erase screen
+                    ILI9341.fillRect(display1, 0, 0, 240, 70, 0xC616);
+                    nextBus -= 1;
+                    if (nextBus <= 2)
+                    {
+                        nextBus = await Utilities.GetNextBus();
+                    }
+
+                    ILI9341.setCursor(display1, 30, 15);
+                    string s = nextBus.ToString() + " min";
+                    ILI9341.write(display1, s.ToCharArray(), 6, 0xDB69);
+                    break;
+            }
+        }
+
+
+        private void CheckAction(int x, int y)
+        {
+            Windows.Foundation.Point touchPoint = new Windows.Foundation.Point(x, y);
+            Rect rect;
+            bool isControlpoint;
+
+            switch (currenPage)
+            {
+                case CurrentPage.Power:
+                    rect = new Rect(64, 53, 108, 110); //Power button is within this rectangle area
+                    isControlpoint = rect.Contains(touchPoint);
+                    if (isControlpoint)
+                    {
+                        ShutDown();
+                    }
+                    break;
+            }
+
+            //Menu controls
+            rect = new Rect(275, 20, 40, 40); //Next bus Icon is within this rectangle area
+            isControlpoint = rect.Contains(touchPoint);
+            if (isControlpoint)
+            {
+                GetNextBus();
+            }
+
+            rect = new Rect(275, 72, 40, 40); //Music player Icon is within this rectangle area
+            isControlpoint = rect.Contains(touchPoint);
+            if (isControlpoint)
+            {
+                PlayMusic();
+            }
+            rect = new Rect(275, 125, 40, 40); //Weather Icon is within this rectangle area
+            isControlpoint = rect.Contains(touchPoint);
+            if (isControlpoint)
+            {
+                GetWeather();
+            }
+
+            rect = new Rect(275, 179, 40, 40);
+            isControlpoint = rect.Contains(touchPoint); //Power button is within this rectangle area
+            if (isControlpoint)
+            {
+                ShowPowerPage();
+            }
+
+        }
+
+        private void ShutDown()
+        {
+
+            ShutdownManager.BeginShutdown(ShutdownKind.Shutdown, TimeSpan.FromSeconds(0.5));
+        }
+
+        private async void CalibrateTouch()
+        {
+            //5 point calibration
+            CAL_POINT[] touchPoints = new CAL_POINT[5];
+            touchPoints[0] = new CAL_POINT();
+            touchPoints[1] = new CAL_POINT();
+            touchPoints[2] = new CAL_POINT();
+            touchPoints[3] = new CAL_POINT();
+            touchPoints[4] = new CAL_POINT();
+
+            CAL_POINT[] screenPoints = new CAL_POINT[5];
+            screenPoints[0] = new CAL_POINT();
+            screenPoints[1] = new CAL_POINT();
+            screenPoints[2] = new CAL_POINT();
+            screenPoints[3] = new CAL_POINT();
+            screenPoints[4] = new CAL_POINT();
+
+            ILI9341.fillRect(display1, 0, 0, 240, 320, 0x0000);
+
+            ILI9341.LineDrawH(display1, 30, 30, 1, 0xFFFF);
+            ILI9341.LineDrawV(display1, 30, 30, 1, 0xFFFF);
+            while (TSC2046.pressure < 5) { TSC2046.CheckTouch(); }//wait for pen pressure
+            screenPoints[0].x = 30;
+            screenPoints[0].y = 30;
+            touchPoints[0].x = TSC2046.tp_x;
+            touchPoints[0].y = TSC2046.tp_y;
+            while (TSC2046.pressure > 1) { TSC2046.CheckTouch(); } // wait for release of pen
+
+
+            ILI9341.LineDrawH(display1, 30, 300, 1, 0xFFFF);
+            ILI9341.LineDrawV(display1, 30, 300, 1, 0xFFFF);
+            while (TSC2046.pressure < 5) { TSC2046.CheckTouch(); }//wait for pen pressure
+            screenPoints[1].x = 300;
+            screenPoints[1].y = 30;
+            touchPoints[1].x = TSC2046.tp_x;
+            touchPoints[1].y = TSC2046.tp_y;
+            while (TSC2046.pressure > 1) { TSC2046.CheckTouch(); }// wait for release of pen
+
+            ILI9341.LineDrawH(display1, 120, 160, 1, 0xFFFF);
+            ILI9341.LineDrawV(display1, 120, 160, 1, 0xFFFF);
+            while (TSC2046.pressure < 5) { TSC2046.CheckTouch(); }//wait for pen pressure
+            screenPoints[2].x = 160;
+            screenPoints[2].y = 120;
+            touchPoints[2].x = TSC2046.tp_x;
+            touchPoints[2].y = TSC2046.tp_y;
+            while (TSC2046.pressure > 1) { TSC2046.CheckTouch(); }// wait for release of pen
+
+
+            ILI9341.LineDrawH(display1, 210, 30, 1, 0xFFFF);
+            ILI9341.LineDrawV(display1, 210, 30, 1, 0xFFFF);
+            while (TSC2046.pressure < 5) { TSC2046.CheckTouch(); }//wait for pen pressure
+            screenPoints[3].x = 30;
+            screenPoints[3].y = 210;
+            touchPoints[3].x = TSC2046.tp_x;
+            touchPoints[3].y = TSC2046.tp_y;
+            while (TSC2046.pressure > 1) { TSC2046.CheckTouch(); }// wait for release of pen
+
+
+            ILI9341.LineDrawH(display1, 210, 300, 1, 0xFFFF);
+            ILI9341.LineDrawV(display1, 210, 300, 1, 0xFFFF);
+            while (TSC2046.pressure < 5) { TSC2046.CheckTouch(); }//wait for pen pressure
+            screenPoints[4].x = 300;
+            screenPoints[4].y = 210;
+            touchPoints[4].x = TSC2046.tp_x;
+            touchPoints[4].y = TSC2046.tp_y;
+            while (TSC2046.pressure > 1) { TSC2046.CheckTouch(); }// wait for release of pen
+
+            TSC2046.setCalibration(screenPoints, touchPoints);
+            if (await TSC2046.CalibrationMatrix.SaveCalData(CalibrationFilename))
+            {
+                //Success
+            }
+            else
+            {
+                //Handle error
+            }
+            ILI9341.Flush(display1);
+        }
+
+        private async void GetWeather()
+        {
+            currenPage = CurrentPage.Weather;
+            //paint Menu
+            await ILI9341.LoadBitmap(display1, 0, 0, 240, 320, "ms-appx:///assets/Home.png");
+
+            //Write loading
+            ILI9341.setCursor(display1, 60, 120);
+            ILI9341.write(display1, "Checking...".ToCharArray(), 2, 0x86D2);
+
+            //Get Weather
+            string responseText = await Utilities.GetjsonStream("http://api.worldweatheronline.com/free/v2/weather.ashx?q=Vienna, Austria&format=JSON&extra=&num_of_days=2&date=&fx=&cc=&includelocation=&show_comments=&callback=&key=4b3c8aaa970207d3e4a5c7fb6d514");
+            LocalWeather localWeather = JsonConvert.DeserializeObject<LocalWeather>(responseText);
+
+            tempC = string.Empty;
+            tempC = localWeather.data.current_Condition[0].temp_C.ToString();
+
+            //Clear
+            ILI9341.fillRect(display1, 0, 0, 240, 150, 0xFFFF);
+
+            //Get weather icon
+            XElement iconXml = XElement.Load("wwoConditionCodes.xml");
+            var iconditions = iconXml.Elements("condition");
+            var iconCodeElement = iconditions.Where(i => i.Element("code").Value == localWeather.data.current_Condition[0].weatherCode).FirstOrDefault();
+
+            string iconFile = string.Empty;
+            if (DateTime.Now.Hour < 18)
+            {
+                iconFile = iconCodeElement.Element("day_icon").Value;
+            }
+            else
+            {
+                iconFile = iconCodeElement.Element("night_icon").Value;
+            }
+
+            iconFile = "ms-appx:///assets/Weather Icons/" + iconFile.Trim() + ".png";
+
+            //Display Icon
+            await ILI9341.LoadBitmap(display1, 60, 20, 120, 120, iconFile);
+
+            if (Convert.ToInt16(tempC) > 9)
+            {
+                ILI9341.setCursor(display1, 80, 150);
+            }
+            else
+            {
+                ILI9341.setCursor(display1, 100, 150);
+            }
+            ILI9341.write(display1, tempC.ToCharArray(), 8, 0xE2C9);
+
+            //Second row can display 20 characters
+            //Calculate spacing
+
+            int count = localWeather.data.current_Condition[0].weatherDesc[0].value.ToString().Length;
+
+            string weatherDesc = localWeather.data.current_Condition[0].weatherDesc[0].value.ToString();
+            UInt16 spacing = 12;
+            if (weatherDesc.Length > 18)
+            {
+                weatherDesc = weatherDesc.Substring(0, 15) + "...";
+            }
+            else
+            {
+                int counter = ((18 - weatherDesc.Length) / 2) + 1;
+                spacing += (UInt16)(spacing * counter);
+            }
+
+            ILI9341.setCursor(display1, spacing, 230);
+            ILI9341.write(display1, weatherDesc.ToCharArray(), 2, 0xE2C9);
+
+        }
+
+        private async void PlayMusic()
+        {
+            currenPage = CurrentPage.MusicPlayer;
+            //paint Menu
+            await ILI9341.LoadBitmap(display1, 0, 0, 240, 320, "ms-appx:///assets/Music_Home.png");
+            likes = await Utilities.GetLikes();
+            if (likes.Count > 0)
+            {
+                LoadTrack(likes[nowPlaying]);
+            }
+        }
+
+        private async void GetNextBus()
+        {
+            currenPage = CurrentPage.NextBus;
+            await ILI9341.LoadBitmap(display1, 0, 0, 240, 320, "ms-appx:///assets/Bus_Home.png");
+            nextBus = await Utilities.GetNextBus();
+            ILI9341.setCursor(display1, 30, 15);
+            string s = nextBus.ToString() + " min";
+            ILI9341.write(display1, s.ToCharArray(), 6, 0xDB69);
+        }
+
+        private async void ShowPowerPage()
+        {
+            currenPage = CurrentPage.Power;
+            await ILI9341.LoadBitmap(display1, 0, 0, 240, 320, "ms-appx:///assets/Power_Home.png");
+        }
+
+
+        private async void Init()
+        {
+            await ILI9341.InitILI9341DisplaySPI(display1, 0, 50000000, SpiMode.Mode0, "SPI0", "");
+            await TSC2046.InitTSC2046SPI();
+            initTimers();
+
+            if (!await TSC2046.CalibrationMatrix.LoadCalData(CalibrationFilename))
+            {
+                CalibrateTouch();
+            }
+
+            //Paint background
+            ILI9341.fillRect(display1, 0, 0, 240, 320, 0xFFFF);
+            ILI9341.Flush(display1);
+
+            GetNextBus();
+
+        }
+
+        private void mPlayer_MediaEnded(System.Object sender, RoutedEventArgs e)
+        {
+            nowPlaying += 1;
+            if (nowPlaying > likes.Count) //Reset to first track
+                nowPlaying = 0;
+            LoadTrack(likes[nowPlaying]);
+        }
+
+        private void LoadTrack(SoundCloudTrack currentTrack)
+        {
+            //Stop player, set new stream uri and play track
+            mPlayer.Stop();
+            Uri streamUri = new Uri(currentTrack.stream_url + "?client_id=YOUR_CLIENT_ID_HERE");
+            mPlayer.Source = streamUri;
+            mPlayer.Play();
+        }
+
+    }
+
+}
+
